9d36085c153bae61ffb2a0a4b737e1ef015828f6
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-29 16:34:49 +0200

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+
+required_conan_version = ">=1.53.0"
+
+
+class TestConan(ConanFile):
+    name = "test"
+    license = "MIT"
+    settings = "os", "arch", "compiler", "build_type"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    generators = "CMakeDeps"
+
+    @property
+    def _min_cppstd(self):
+        return 17
+
+    def requirements(self):
+        self.requires("fmt/10.2.1#9199a7a0611866dea5c8849a77467b25")
+
+    def validate(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+    def configure(self):
+        if self.settings.compiler in ["clang", "gcc"]:
+            self.settings.compiler.libcxx = "libstdc++11"
+
+        self.options["fmt"].header_only = True
